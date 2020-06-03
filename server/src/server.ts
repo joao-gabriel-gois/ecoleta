4752b805dbceeab93f56d591cb0299899890e86b
Joao Gabriel <thereis@noone.coml>
2020-06-03 05:15:31 -0300

    Adding CORS support

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
 const app = express();
 
 //Middlewares
-app.use(routes);
+app.use(cors());
 app.use(express.json())
+app.use(routes);
 
 //Static
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
