d143f57dd4b72be3f440ee7edd0f2d25988920a0
Joao Gabriel <thereis@noone.coml>
2020-06-05 03:50:25 -0300

    Map and position handled

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
 import axios from 'axios';
 
@@ -27,12 +28,22 @@ const CreatePoint = () => {
    // states for arrays and obj shoud have a type of elements
    const [ items, setItems ] = useState<Item[]>([]); // it could be useState<Array<Item>>([]) also
    const [ ufs, setUfs ] = useState<string[]>([]);
-   const [ selectedUf, setSelectedUf ] = useState<string>('0');
-   const [ selectedCity, setSelectedCity ] = useState<string>('0');
-
    const [ cities, setCities ] = useState<string[]>([]);
 
+   const [ selectedUf, setSelectedUf ] = useState<string>('0');
+   const [ selectedCity, setSelectedCity ] = useState<string>('0');
+   
+   const [ initialPosition, setInitialPosition ] = useState<[number, number]>([0, 0]);
+   const [ selectedPosition, setSelectedPosition ] = useState<[number, number]>([0, 0]);
 
+//get initial position
+   useEffect(() =>{
+      navigator.geolocation.getCurrentPosition(position => {
+         const { latitude, longitude } = position.coords;
+         setInitialPosition([latitude, longitude]);
+      })
+   }, [])
+//get items
    useEffect(() => {
       api.get('items').then(response => {
          
@@ -45,7 +56,7 @@ const CreatePoint = () => {
       
       })
    }, [])
-
+//get UF from IBGE
    useEffect(() => {
       axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
          .then(response => {
@@ -59,7 +70,7 @@ const CreatePoint = () => {
       
          })
    }, [])
-
+//get Cities every time seceted UF changes
    useEffect(() => {
       if (selectedUf === '0') return;
 
@@ -82,6 +93,13 @@ const CreatePoint = () => {
 
    function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
       setSelectedCity(event.target.value);
+   }
+
+   function handleMapClick(event: LeafletMouseEvent) {
+      setSelectedPosition([
+         event.latlng.lat,
+         event.latlng.lng,
+      ]);
    }
 
    return (
@@ -138,13 +156,13 @@ const CreatePoint = () => {
                   <span>Selecione o endereço no mapa</span>
                </legend>
 
-               <Map center={[-27.2092052, -49.6401092]} zoom={15}>
+               <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
                   <TileLayer 
                      attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   />
                   <Marker
-                     position={[-27.2092052, -49.6401092]}
+                     position={selectedPosition}
                   />
                </Map>
 
