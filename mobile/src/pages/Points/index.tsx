84385d37388101a7a7710d8b91fea6ed53d457c3
Joao Gabriel <thereis@noone.coml>
2020-06-07 00:06:06 -0300

    mobile - fecthing items from api, done

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,73 +1,109 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Feather as Icon } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, ScrollView, SafeAreaView, View, Text, Image, TouchableOpacity} from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
+import api from '../../services/api';
+
+interface Item {
+  id: number,
+  title: string,
+  image_url: string
+}
 
 const Points = () => {
-    const navigation = useNavigation();
+  const [ items, setItems ] = useState<Item[]>([])
+  const [ selectedItems, setSelectedItems ] = useState<number[]>()
+  const navigation = useNavigation();
 
-    function handleNavigateBack() {
-        navigation.goBack();
+  useEffect(() => {
+    api.get('items').then(response => {
+      setItems(response.data);
+    });
+  }, [])
+
+  function handleNavigateBack() {
+      navigation.goBack();
+  }
+
+  function handleNavigateToDetail() {
+      navigation.navigate('Detail')
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelectedItem = selectedItems.findIndex(item => item == id);
+    // toggle logic bellow
+    if (alreadySelectedItem >= 0) {
+       const filteredItems = selectedItems.filter(item => item !== id);
+       
+       setSelectedItems(filteredItems);
+    } else {
+       setSelectedItems([ ...selectedItems, id ]);
     }
+ }
 
-    function handleNavigateToDetail() {
-        navigation.navigate('Detail')
-    }
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34CB79" />
+        </TouchableOpacity>
 
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={styles.container}>
-          <TouchableOpacity onPress={handleNavigateBack}>
-            <Icon name="arrow-left" size={20} color="#34CB79" />
-          </TouchableOpacity>
+        <Text style={styles.title}>Bem vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta</Text>
 
-          <Text style={styles.title}>Bem vindo.</Text>
-          <Text style={styles.description}>Encontre no mapa um ponto de coleta</Text>
-
-          <View style={styles.mapContainer}>
-            <MapView
-              style={styles.map}
-              initialRegion={{
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: -27.2092052,
+              longitude: -49.6401092,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }}
+          >
+            <Marker
+              style={styles.mapMarker}
+              onPress={handleNavigateToDetail}
+              coordinate={{
                 latitude: -27.2092052,
                 longitude: -49.6401092,
-                latitudeDelta: 0.014,
-                longitudeDelta: 0.014,
-              }}
-            >
-              <Marker
-                style={styles.mapMarker}
-                onPress={handleNavigateToDetail}
-                coordinate={{
-                  latitude: -27.2092052,
-                  longitude: -49.6401092,
-              }}>
-                  <View style={styles.mapMarkerContainer}>
-                    <Image
-                      style={styles.mapMarkerImage}
-                      source={{uri:'https://pt.freelogodesign.org/Content/img/logo-samples/bakary.png'}}
-                    />
-                    <Text style={styles.mapMarkerTitle}>Mercado</Text>
-                  </View>
-              </Marker>
-            </MapView>
-          </View>
+            }}>
+                <View style={styles.mapMarkerContainer}>
+                  <Image
+                    style={styles.mapMarkerImage}
+                    source={{uri:'https://pt.freelogodesign.org/Content/img/logo-samples/bakary.png'}}
+                  />
+                  <Text style={styles.mapMarkerTitle}>Mercado</Text>
+                </View>
+            </Marker>
+          </MapView>
         </View>
+      </View>
+      <View style={styles.itemsContainer}>
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={{ padding: 20 }}  
         >
-          <View style={styles.itemsContainer}>
-            <TouchableOpacity style={styles.item} onPress={()=>{}}>
-              <SvgUri width={42} height={42} uri="http://192.168.1.121:3333/uploads/lampadas.svg" />
-              <Text style={styles.itemTitle}>Lampadas</Text>
+        {items ? 
+          items.map(item => (
+            <TouchableOpacity
+              key={String(item.id)}
+              style={styles.item}
+              activeOpacity={0.6}
+              onPress={() => handleSelectItem(item.id)}
+            >
+              <SvgUri width={42} height={42} uri={item.image_url} />
+              <Text style={styles.itemTitle}>{item.title}</Text>
             </TouchableOpacity>
-          </View>
+          )) : ''
+        }
         </ScrollView>
-      </SafeAreaView>
-    )
+      </View>  
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
